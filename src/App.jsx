663f843c10ca3de846228a48955ceaf9f3b90efb
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-01-30 21:19:03 +0530

    final commmit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Model from './components/Model'
 import Features from './components/Features'
 import HowItWorks from './components/HowItWorks'
 import Footer from './components/Footer'
-
+import { Analytics } from "@vercel/analytics/react"
 const App = () => {
   return (
     <main className='bg-black'>
@@ -17,6 +17,7 @@ const App = () => {
       <Features/>
       <HowItWorks/>
       <Footer/>
+      <Analytics/>
     </main>
   )
 }
